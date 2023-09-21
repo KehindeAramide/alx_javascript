c28130b6a66a22cbf3f31e28c0975fba35dfa21f
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 11:07:07 +0100

    task6

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,3 +1,5 @@
-exports.callMemoby = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
     for (let i = 0; i < x; i++){
-        theFunction();}};
+        theFunction();
+    }
+};
