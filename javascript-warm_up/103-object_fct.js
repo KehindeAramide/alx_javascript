6351a0aac78608c4f4ebd7c30e1d7de837878887
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 11:28:14 +0100

    task8

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,17 +1,19 @@
 //Update this script by adding a new function incr that increments the integer value
 
-const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
+const myVar = {
+  type: 'object',
+  value: 12,
+  incr: function() {
     this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  }
+};
+
+console.log(myVar);
+myVar.incr();
+console.log(myVar);
+myVar.incr();
+console.log(myVar);
+myVar.incr();
+console.log(myVar);
+
 
