263d9f841e1e9ddf1b3f219bbfb43cce52e674fc
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 10:48:07 +0100

    task3

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,10 +1,12 @@
 // Writing this script to replaces the value of 12 with 89
 
 const myObject = {
-   type: 'object',
-   value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject.value);
-  
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject); // Updated object with the new value
