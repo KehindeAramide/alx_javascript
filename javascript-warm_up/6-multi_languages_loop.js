f0d6d9a38d1049a2a2b6c68ee995cfcfea64c1f7
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 09:26:06 +0100

    task2

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+//Writes a script that prints 3lines by using an array of string and loop
+
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const line of lines) {
+    console.log(line);
+}
