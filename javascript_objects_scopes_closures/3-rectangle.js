49d69002c30517131bfb88c9c1ff4dd9c131bc35
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 13:20:51 +0100

    task3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+            // Create an empty object if w or h is equal to 0 or not a positive integer
+            return {};
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+}
+
+const rect = new Rectangle(3, 4);
+rect.print();
