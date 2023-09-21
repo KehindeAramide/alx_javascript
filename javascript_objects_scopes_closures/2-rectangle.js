d607fa5dc1fc77999b331d644ed7fd82bb8c2acc
KehindeAramide <kenned2k4@yahoo.com>
2023-09-21 13:30:39 +0100

    task2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,11 @@
 //This code defines a Rectangle class with the specified constructor and attributes.
-#!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-            // Create an empty object if w or h is equal to 0 or not a positive integer
-            return {};
-        }
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
         this.width = w;
         this.height = h;
+      }
     }
-}
-
-const rect = new Rectangle(3, 3);
-console.log(rect); // Outputs: Rectangle { width: 3, height: 3 }
-console.log(rect.width); // Outputs: 3
-console.log(rect.height); // Outputs: 3
+  }
+  
+  module.exports = Rectangle;
