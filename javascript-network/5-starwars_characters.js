d1b663d594b61feb787466d08181a43d61b2f17f
KehindeAramide <kenned2k4@yahoo.com>
2023-09-22 18:24:15 +0100

    task5

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,50 +1,20 @@
-//
-const request = require('request');
-
-const movieId = 1; // Replace with the desired movie ID
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
+//Writes a script that prints all characters of a Star Wars movie
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-
-  // Extract character URLs from the movie data
-  const characterUrls = movieData.characters;
-
-  // Function to fetch and print character names
-  function fetchAndPrintCharacterNames(urls) {
-    if (urls.length === 0) {
-      return;
-    }
-
-    const characterUrl = urls.pop();
-    request(characterUrl, (error, response, characterBody) => {
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
       if (error) {
-        console.error(error);
-      } else if (response.statusCode === 200) {
-        const characterData = JSON.parse(characterBody);
-        console.log(characterData.name);
+        console.log(error);
       }
-
-      // Continue with the next character URL
-      fetchAndPrintCharacterNames(urls);
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
   }
-
-  // Divide the character URLs into two arrays based on the API response structure
-  const characterUrlsPart1 = characterUrls.slice(0, 10);
-  const characterUrlsPart2 = characterUrls.slice(10);
-
-  // Fetch and print characters from both parts
-  fetchAndPrintCharacterNames(characterUrlsPart1);
-  fetchAndPrintCharacterNames(characterUrlsPart2);
 });
