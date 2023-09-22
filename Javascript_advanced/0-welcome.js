9e8bcc9608c811146917fd19a27f30a9fd3f7a97
KehindeAramide <kenned2k4@yahoo.com>
2023-09-22 18:48:29 +0100

    task0

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,14 @@
+//
+function welcome(firstName, lastName) {
+    const fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+  
+    displayFullName();
+  }
+  
+  // Example usage:
+  welcome('Holberton', 'School');
+  
