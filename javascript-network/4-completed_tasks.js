fa6f96fe5f8d88e1016178a3ee11c143f806e576
KehindeAramide <kenned2k4@yahoo.com>
2023-09-22 18:22:27 +0100

    task4 upd

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,43 +1,25 @@
 //Writes a script that computes the number of tasks completed by user id.
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = 'http://localhost:5050/route_1';
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode);
-    return;
-  }
-
-  const data = JSON.parse(body);
-
-  // Create an object to store the counts of completed tasks for each user
-  const userCompletedTasks = {};
-
-  // Loop through the data and count completed tasks for each user
-  data.forEach((item) => {
-    const userId = item.userId;
-    if (item.completed) {
-      if (userCompletedTasks[userId]) {
-        userCompletedTasks[userId]++;
-      } else {
-        userCompletedTasks[userId] = 1;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-  });
-
-  // Filter out users with 0 completed tasks
-  const filteredUserCompletedTasks = {};
-  for (const userId in userCompletedTasks) {
-    if (userCompletedTasks[userId] > 0) {
-      filteredUserCompletedTasks[userId] = userCompletedTasks[userId];
-    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-
-  console.log(filteredUserCompletedTasks);
 });
